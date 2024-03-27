a04f8c4298f3b859bd40ee87214932df80c2511e
Anushka-gairola <Anushkagairola12@gmail.com>
2024-03-27 19:46:11 +0530

    testing commit changes

--- calculator.py
+++ calculator.py
@@ -1,11 +1,9 @@
 #Taking the input from user 
-
 first = input ("enter 1st number: ")
 operator = input( "enter operator (+, -,*,/,%)")
 second= input("enter 2nd number: ")
 
 #Converting srting into numbers 
-
 first= int(first)
 second=int(second)
 
@@ -14,7 +12,6 @@ if operator == "+":
     print(first + second)
 
 #Using elif statement (the next condition will run only of the above condition is false )
-    
 elif operator == "-":
     print(first - second)
 elif operator== "*":
